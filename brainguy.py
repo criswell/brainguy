adcf7aff8895d0b1b99d86bfecd95140a0214875
Sam Hart <hartsn@gmail.com>
2013-12-02 12:18:24 -0500

    requirements and basic poc

--- brainguy.py
+++ brainguy.py
@@ -5,23 +5,37 @@
    Fix MST3k nfo lookup files
 """
 
-import os.path
+from __future__ import print_function
+
+import os.path, sys, os
 
 import imdb
 
+if len(sys.argv) < 2:
+    print "-= Need to specify the path to MST3k root folder"
+    sys.exit(1)
+
 mst3k_id = '0094517'
 
-print "-= Connecting to IMDb"
+print("-= Connecting to IMDb")
 
 i = imdb.IMDb()
 m = i.get_movie(mst3k_id)
 
 episodes = dict()
-lookup = []
+titles = []
 
-print "-= Updating all episodes..."
+print("-= Updating all episodes...")
 i.update(m, 'episodes')
 
 for season in m['episodes']:
     for e in season:
-        i = 1
+        episodes[e['title']] = e
+        titles.append(e['title'].lower())
+
+mst3k_path = sys.argv[1]
+
+print("-= Getting list of files from '%s'" % mst3k_path)
+
+for root, dirs, files in os.walk(mst3k_path):
+    foo
