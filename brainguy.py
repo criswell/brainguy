088da392250cbcc844414ace6e2707d5173d97bb
Sam Hart <hartsn@gmail.com>
2013-12-02 19:50:58 -0500

    start generalizing

--- brainguy.py
+++ brainguy.py
@@ -2,40 +2,74 @@
 # -*- coding: utf-8 -*-
 
 """
-   Fix MST3k nfo lookup files
+   Fix Series nfo lookup files
 """
 
 from __future__ import print_function
 
-import os.path, sys, os
+try:
+    import ConfigParser as configparser
+except ImportError:
+    import configparser
+
+import os.path, sys, os, argparse
 
 import imdb
 
-if len(sys.argv) < 2:
-    print "-= Need to specify the path to MST3k root folder"
-    sys.exit(1)
+parser = argparse.ArgumentParser(
+    description = 'Fixes series nfo lookup files.')
+parser.add_argument('paths', nargs='+',
+    help='Path or paths to directories to process.')
 
-mst3k_id = '0094517'
+args = parser.parse_args()
 
-print("-= Connecting to IMDb")
+supported_types = [
+    'series',
+    'movie'
+]
 
-i = imdb.IMDb()
-m = i.get_movie(mst3k_id)
+def process_series(root, files):
+    root_id = None
+    if config.has_option('hint', 'root_id'):
+        root_id = config.get('hint', 'root_id')
 
-episodes = dict()
-titles = []
+    print("-= Connecting to IMDb")
 
-print("-= Updating all episodes...")
-i.update(m, 'episodes')
+    i = imdb.IMDb()
+    m = i.get_movie(root_id)
 
-for season in m['episodes']:
-    for e in season:
-        episodes[e['title']] = e
-        titles.append(e['title'].lower())
+    episodes = dict()
+    titles = []
 
-mst3k_path = sys.argv[1]
+    print("-= Updating all episodes...")
+    i.update(m, 'episodes')
 
-print("-= Getting list of files from '%s'" % mst3k_path)
+    for season in m['episodes']:
+        for e in season:
+            episodes[e['title']] = e
+            titles.append(e['title'].lower())
 
-for root, dirs, files in os.walk(mst3k_path):
-    foo
+
+for p in args.paths:
+    for root, dirs, files in os.walk(p):
+        if '.hint' in files:
+            config = configparser.SafeConfigParser()
+            config.readfp(open(os.path.abspath('%s/.hint' % root)))
+            show_type = 'series'
+            if config.has_option('hint', 'type'):
+                show_type = config.get('hint', 'type')
+            if show_type not in supported_types:
+                print('> Error in config, unknown type "%s"' % show_type)
+                show_type = supported_types[0]
+                print('>-- Using type "%s"' % show_type)
+
+            if show_type is 'series':
+                process_series(root, files)
+
+
+#mst3k_path = sys.argv[1]
+
+#print("-= Getting list of files from '%s'" % mst3k_path)
+
+#for root, dirs, files in os.walk(mst3k_path):
+#    foo
