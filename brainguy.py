83f8b447f274694fa3fd35d28abed0118b0bfb29
Sam Hart <hartsn@gmail.com>
2013-12-02 22:22:22 -0500

    tvdb suppot

--- brainguy.py
+++ brainguy.py
@@ -15,6 +15,7 @@ except ImportError:
 import os.path, sys, os, argparse, re
 
 import imdb
+from pytvdbapi import api
 
 parser = argparse.ArgumentParser(
     description = 'Fixes series nfo lookup files.')
@@ -28,41 +29,82 @@ supported_types = [
     'movie'
 ]
 
+global_config = None
+try:
+    global_config = configparser.SafeConfigParser()
+    global_config.readfp('%s/.brainguy' % os.path.expanduser("~"))
+    tvdb_api = None
+    if global_config.has_option('global', 'api_key'):
+        tvdb_api = global_config.get('global', 'api_key')
+except:
+    print('-= Problem parsing ~/.brainguy, no global settings used')
+
+def get_info_from_file(f):
+    result = re.findall(r"(?:s|season)(\d{2})(?:e|x|episode|\n)(\d{2})", f, re.I)
+    season = None
+    episode = None
+    if len(result) > 0:
+        season = int(result[0][0])
+        episode = int(result[0][1])
+    return (season, episode)
+
 def process_series(root, files):
     root_id = None
     if config.has_option('hint', 'root_id'):
         root_id = config.get('hint', 'root_id')
 
+    service = 'imdb'
+    if config.has_option('hint', 'service'):
+        service = config.get('hint', 'service')
+
+    if service == 'imdb':
+        process_series_imdb(root_id, root, files)
+    elif service == 'tvdb':
+        process_series_tvdb(root_id, root. files)
+    else:
+        print('-= ERROR! Service id "%s" unknown, skipping' % service)
+
+def write_nfo_file(root, f, url):
+    file_pre = ''.join(f.split('.')[:-1])
+    nfo_fname = '%s/%s.nfo' % (root, file_pre)
+    print('>--- Writing "%s"' % nfo_fname)
+    with open(nfo_fname, 'wb') as outfile:
+        outfile.write('%s\n' % url)
+
+def process_series_tvdb(root_id, root, files):
+    print('-= Connecting to the TVDB')
+    db = api.TVDB(tvdb_api)
+    series = db.get_series(root_id, 'en')
+    if len(series) > 0:
+        for f in files:
+            (season, episode) = get_info_from_file(f)
+            if episode is not None:
+                try:
+                    e = series[season][episode]
+                    url = 'http://www.thetvdb.com/?tab=episode&seriesid=%d&seasonid=%d&id=%d' % (
+                        root_id, e.seasonid, e.id)
+                    write_nfo_file(root, f, url)
+                except:
+                    print('-= ERROR! Processing s%de%d' % (season, episode))
+    else:
+        print('-= ERROR! Could not find series "%s"' % root_id)
+
+def process_series_imdb(root_id, root, files):
     print("-= Connecting to IMDb")
 
     i = imdb.IMDb()
     m = i.get_movie(root_id)
 
-    #episodes = dict()
-    #titles = []
-
     print("-= Updating all episodes...")
     i.update(m, 'episodes')
 
-    #for season in m['episodes']:
-    #    for e in m['episodes'][season]:
-    #        episodes[e['title']] = e
-    #        titles.append(e['title'].lower())
-
     for f in files:
-        result = re.findall(r"(?:s|season)(\d{2})(?:e|x|episode|\n)(\d{2})", f, re.I)
-        if len(result) > 0:
-            season = int(result[0][0])
-            episode = int(result[0][1])
+        (season, episode) = get_info_from_file(f)
+        if episode is not None:
             try:
                 e = m['episodes'][season][episode]
                 url = 'http://www.imdb.com/title/tt%s/' % e.movieID
-                file_pre = ''.join(f.split('.')[:-1])
-                nfo_fname = '%s/%s.nfo' % (root, file_pre)
-                #import pdb; pdb.set_trace()
-                print('>--- Writing "%s"' % nfo_fname)
-                with open(nfo_fname, 'wb') as outfile:
-                    outfile.write('%s\n' % url)
+                write_nfo_file(root, f, url)
             except:
                 print('>--- ERROR processing s"%d" e"%d"' % (season, episode))
 
